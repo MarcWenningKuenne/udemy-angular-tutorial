45e1124c4a28228c1fd8af2b75a2571a59abb6b0
Marc Wenning-Künne <marcwenningkuenne@gmail.com>
2021-06-28 21:39:26 +0200

    Kapitel 23 - Property Binding in der Component DatabindingComponent

--- src/app/databinding/databinding.component.ts
+++ src/app/databinding/databinding.component.ts
@@ -1,16 +1,26 @@
+import { preserveWhitespacesDefault } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-databinding',
   templateUrl: './databinding.component.html',
+  styles: [`
+    .red-border{
+      border: 1px solid red;
+    }
+  `]
 })
 export class DatabindingComponent{
   aString = 'Ich bin ein String';
   aNumber = 100;
+  attachClass = false;
+  attachColor = 'white';
 
   constructor(){
     setTimeout(() => {
       this.aNumber += 200;
+      this.attachClass = true;
+      this.attachColor = 'blue';
     },3000)
   }
 }
