f3c4d53c408919cbdfb98e8b47573e84f1fc985d
Marc Wenning-Künne <marcwenningkuenne@gmail.com>
2021-06-28 20:32:08 +0200

    Kapitel 17 - Other Component and Another Component erstellt und eingebunden

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { OtherComponent } from './other/other.component';
+import { AnotherComponent } from './other/another.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OtherComponent
+    OtherComponent,
+    AnotherComponent
   ],
   imports: [
     BrowserModule
