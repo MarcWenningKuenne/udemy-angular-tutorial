506c3aae4fd40b02a113d869871bd98205f14dd4
Marc Wenning-Künne <marcwenningkuenne@gmail.com>
2021-06-28 22:11:13 +0200

    Kapitel 26 - Eigene Events schaffen und binden. Eigenes Click Event in EventBindingComponent geschaffen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DatabindingComponent } from './databinding/databinding.component';
+import { PropertyBindingComponent } from './databinding/property-binding.component';
+import { EventBindingComponent } from './databinding/event-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DatabindingComponent
+    DatabindingComponent,
+    PropertyBindingComponent,
+    EventBindingComponent
   ],
   imports: [
     BrowserModule
