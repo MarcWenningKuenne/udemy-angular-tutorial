765f283a634edbc412f715d037737ccabedd8dcc
Marc Wenning-Künne <marcwenningkuenne@gmail.com>
2021-06-28 20:54:16 +0200

    Kapitel 19 - <ng-content> Zum maipulieren des Inhaltes einer Component durch das Template einer anderen Component. another.component.ts und other.component.html

--- src/app/other/another.component.ts
+++ src/app/other/another.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
     <p>
       another works!
     </p>
+    <ng-content></ng-content>
   `,
+  // mit <ng-content></ng-content> gibt man an, dass im template einer anderen
+  // Component der Inhalt dieser Component verändert werden kann. Dies
+  // geschieht in der Datein other.component.html
   styles: [
   ]
 })
