2fa477ff46dcc6c412e788b72619e40086b151e2
Marc Wenning-Künne <marcwenningkuenne@gmail.com>
2021-06-29 08:22:48 +0200

    Kapitel 31 - Zurücksetzen der app auf den Anfangsstand

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { OtherComponent } from './other/other.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    OtherComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
