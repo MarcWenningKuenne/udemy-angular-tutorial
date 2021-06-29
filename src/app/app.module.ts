760da06efc57d03d373a0ecf072a0d90aa11b46a
Marc Wenning-Künne <marcwenningkuenne@gmail.com>
2021-06-29 10:10:04 +0200

    Initiales anlegen der Components für die Shopping-List und Recipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    RecipeComponent,
+    ShoppingListComponent,
   ],
   imports: [
     BrowserModule
