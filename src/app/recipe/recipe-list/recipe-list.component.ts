b4b995dc4d08e8159e42d9a8e31a9a9dff4321eb
Marc Wenning-Künne <marcwenningkuenne@gmail.com>
2021-06-29 10:34:57 +0200

    Kapitel 37 - Anlegen des recipe.model

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class RecipeListComponent implements OnInit {
-
+  recipe = new Recipe('Dummy', 'Dummy', 'https://media.fortniteapi.io/images/51d30a24ab77d09644e0db6a9ea14ddc/transparent.png')
   constructor() { }
 
   ngOnInit(): void {
