b00c9181534be3b2ccc8cbcc4e2afbc52d8cb2aa
Marc Wenning-Künne <marcwenningkuenne@gmail.com>
2021-06-29 09:57:27 +0200

    Kapitel 34 - Aufsetzen eines neuen Projektes für die App Rezeptbuch. Es wurde bereits bootstrap eingebunden.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>Databinding</h1>
-    <app-lifecycle [name]="name" *ngIf="attach"></app-lifecycle>
-    <hr>
-    <button (click)="name = 'Anna'">Name ändern</button>
-    <button (click)="attach = false">Zerstören</button>
-  `
+  templateUrl: './app.component.html' 
 })
 export class AppComponent {
-  name = 'Marc';
-  attach = true;
+
 }
