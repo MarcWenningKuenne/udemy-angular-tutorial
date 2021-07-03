1b6bf2bd9eb56f4b5d44e161af3e8bb1f9e5502f
Marc Wenning-Künne <marcwenningkuenne@gmail.com>
2021-07-03 20:39:38 +0200

    Routing aufgesetzt in app.routing.ts, app.module.ts und app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RecipeItemComponent } from './recipe/recipe-list/recipe-item.component'
 import { ShoppingListAddComponent } from './shopping-list/shopping-list-add.component';
 import { DropwdownDirective } from './dropwdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
     DropwdownDirective,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
