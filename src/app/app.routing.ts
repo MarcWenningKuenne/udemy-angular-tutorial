e7b9ee7ec2a9a35ece54c6cff020ce8ca45c9ea4
Marc Wenning-Künne <marcwenningkuenne@gmail.com>
2021-07-03 20:45:12 +0200

    routerLink in Header für das routing in der Menü Struktur

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,7 +3,7 @@ import { RecipeComponent } from "./recipe/recipe.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 const APP_ROUTES: Routes = [
-    {path: '', redirectTo: '/rezepte'},
+    {path: '', redirectTo: '/rezepte', pathMatch: 'full'},
     {path: 'rezepte', component: RecipeComponent},
     {path: 'einkaufsliste', component: ShoppingListComponent}
 ]
